eeecf59985aae2824a3b7b6c3dc15d39d606a047
Eduard Ballweg <eduard.ballweg@gmail.com>
2025-12-18 18:38:23 +0100

    mit vereinen

---
+++ railway app4.session.sql
@@ -0,0 +1,11 @@
+/*CREATE TABLE zeiten (
+    id SERIAL PRIMARY KEY,
+    zeit TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE TABLE vereine (
+    id SERIAL PRIMARY KEY,
+    vereinsname TEXT NOT NULL
+);
+
+*/
